681301c48c0ab7157116383385f097c986330963
daniel211099 <daniel.alf@htwg-konstanz.de>
2022-06-06 23:01:36 +0200

    Revert "Removed death code in main.c"
    This reverts commit 49af8e71e695cc40af314b37f4060c3adb03ade2.
    
    Fixed issue and removed mjson

--- ProgrammHandler.h
+++ ProgrammHandler.h
@@ -9,7 +9,6 @@
 #include <string.h>
 #include "mongoose/mongoose.h"
 #include "utils/uartstdio.h"
-#include "mjson/mjson.h"
 #include "CFAF128128B0145T/CFAF128128B0145T.h"
 #include "CFAF128128B0145T/CFAF128128B0145T_font6x8.h"
 #include "inc/hw_memmap.h"
